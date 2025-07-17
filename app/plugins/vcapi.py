6437bdf17cd91b3ffc3bd472cdf826ff13098350
PatStLouis <patrick.st-louis@opsecid.ca>
2025-07-17 20:10:32 +0000

    add comments
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- app/plugins/vcapi.py
+++ app/plugins/vcapi.py
@@ -45,10 +45,14 @@ class VcApiExchanger:
         profile = await askar.fetch("profile", self.client_id)
         wallet_id = profile.get('wallet_id')
         wallet = await askar.fetch("wallet", wallet_id)
+        
+        # Start building presentation object
         presentation = {
             '@context': ['https://www.w3.org/ns/credentials/v2'],
             'type': ['VerifiablePresentation'],
         }
+        
+        # Define proof options
         proof_options = {
             # TODO, how to select cryptosuite? Default to 'Ed25519Signature2020' for now
             'proofType': 'Ed25519Signature2020',
@@ -62,6 +66,7 @@ class VcApiExchanger:
         credentials = await askar.fetch("credentials", wallet_id)
         for query in vpr.get('query'):
             if query.get('type') == 'DIDAuthentication':
+                
                 methods = [method['method'] for method in query.get('acceptedMethods')]
                 
                 # TODO, only DID key for now, add support for did web
@@ -73,14 +78,20 @@ class VcApiExchanger:
                 proof_options['verificationMethod'] = f'did:key:{multikey}#{multikey}'
                 
             if query.get('type') == 'QueryByExample':
+                
+                # We add the verifiableCredential property if not present
+                if not presentation.get('verifiableCredential'):
+                    presentation['verifiableCredential'] = []
+
                 cred_queries = query.get('credentialQuery')
                 cred_queries = cred_queries if isinstance(cred_queries, list) else [cred_queries]
                 for cred_query in cred_queries:
 
-                    # Check if the requested credential is required
+                    # Check if the requested credential is required, ignore if optional...
                     if not cred_query.get('required', True):
                         continue
                     
+                    # TODO, process required trusted issuers
                     # trusted_issuers = cred_query.get('trustedIssuer', [])
                     reason = cred_query.get('reason')
                     example = cred_query.get('example')
@@ -109,10 +120,6 @@ class VcApiExchanger:
                     
                     # We select the first filtered proof to include in the presentation
                     vc['proof'] = proofs[0]
-
-                    # We add the verifiableCredential property if not present
-                    if not presentation.get('verifiableCredential'):
-                        presentation['verifiableCredential'] = []
                         
                     # We append our credential matching the requested query
                     presentation['verifiableCredential'].append(vc)
@@ -124,7 +131,6 @@ class VcApiExchanger:
         token = agent.request_token(wallet_id, wallet.get('wallet_key')).get('token')
         agent.set_token(token)
         vp = agent.sign_presentation(presentation, proof_options).get('verifiablePresentation')
-        print(vp)
         r = requests.post(self.exchange_url, json=vp)
         
         # TODO, fix presentation
